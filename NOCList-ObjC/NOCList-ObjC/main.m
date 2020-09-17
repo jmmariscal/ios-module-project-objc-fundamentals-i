7f7be9518b6d3c0acc6226e63f2150d5d723d92c
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-09-16 18:57:01 -0700

    First commit

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Juan M Mariscal on 9/16/20.
+//  Copyright © 2020 Juan M Mariscal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
